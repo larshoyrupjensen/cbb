7caed1318031978f7c20d4bd8c20f89f3d56a613
larshoyrupjensen <lars.hoyrup.jensen@gmail.com>
2017-12-22 22:46:13 +0100

    a lot of code cleanup

--- cbb/spiders/mobiler.py
+++ cbb/spiders/mobiler.py
@@ -1,20 +1,12 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from scrapy.shell import inspect_response
 from scrapy_splash import SplashRequest
 import unicodedata
-#from scrapy.mail import MailSender
-import locale
-import os
-from . import send_email
-from .tools import find_changed_phones
-import json
+from .send_email import send_email
+from .tools import find_changed_phones, phone_sorter, load_phones, save_phones
 import datetime
 import pandas as pd
 
-#This doesn't work on Linux, so disabled
-#locale.setlocale(locale.LC_ALL, 'Danish')
-print(os.path.abspath(os.path.curdir))
 
 
 class MobilerSpider(scrapy.Spider):
@@ -22,7 +14,7 @@ class MobilerSpider(scrapy.Spider):
     allowed_domains = ['cbb.dk']
     start_urls = ['https://www.cbb.dk/']
     timestamp=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    phones=[]
+    phones=[] #List to hold scraped phones
 
     def start_requests(self):
         script = """
@@ -39,49 +31,45 @@ class MobilerSpider(scrapy.Spider):
                                 args={"lua_source": script})
 
     def closed(self, spider):       
-        #sort scraped phones by 1) brand and 2) price
-        def phone_sorter(phone):
-            if phone["price"] == None:
-                return (phone["brand"], 0)
-            else:
-                return (phone["brand"], phone["model"], phone["price"])
+        #On shutdown, sort scraped phones by 1) brand, 2) model and 3) price
         self.phones = sorted(self.phones, 
                              key = phone_sorter)
-                             #key = lambda k: (k["brand"], k["price"]))
+
         df = pd.DataFrame(self.phones)
         d1 = dict(selector="th", props=[('text-align', 'left')])
         d2 = dict(selector="style", props=[('text-align', 'left')])
         df.index = df.index + 1
-        html = df.style.set_table_styles([d1, d2])\
+        new_phones_as_html = df.style.set_table_styles([d1, d2])\
             .render()
             #.set_properties(**{'text-align': 'left', 'width':'10cm'})\
-        #new_phones_as_html = df.to_html(border=0)
-        new_phones_as_html = html
         with open("new_phones_table.html", "w") as fp:
             fp.write(new_phones_as_html)
 
-        phones=load_phones()
-        changes=find_changed_phones(self.phones, phones)
+        old_phones=load_phones()
+        changes=find_changed_phones(self.phones, old_phones)
         df = pd.DataFrame(changes)
         changes_as_html = df.to_html(col_space=100, border=0)
-        phones=phones + self.phones
-        save_phones(phones)
-        body="ÆNDREDE TELEFONER\n\n"
-        body = ""
-        body += changes_as_html
-        #body += "\n\nSKRABEDE TELEFONER\n\n"
+        all_phones = old_phones + self.phones
+        save_phones(all_phones)
+        body = changes_as_html
         body += new_phones_as_html
-        send_email.send_email(
+        send_email(
                 content=body, 
                 recipient="lars.hoyrup.jensen@gmail.com",
                 subject="CBB spider kørt {}".format(self.timestamp))
         
     def parse(self, response):
+        #The Lua script below is used by parse_list_of_mobiles()
+        #It clicks "Vis flere" button until error which is caught. 
+        #Then we are sure all phones are shown on page
         script = """
             function main(splash, args)
               assert(splash:go(args.url))
               assert(splash:wait(2.5))
-              element = splash:select("#menufication-page-holder > div.landingpage.item-template.overview-template > div.products-section > div > div > div.col-xs-12.text-center > button")
+              element = splash:select("#menufication-page-holder > \
+                  div.landingpage.item-template.overview-template > \
+                  div.products-section > div > div > \
+                  div.col-xs-12.text-center > button")
               more_to_show = true
               while more_to_show do
               	if pcall(show_more, splash, element) then
@@ -101,17 +89,20 @@ class MobilerSpider(scrapy.Spider):
               splash:wait(2.5)
             end  
         """
-        #inspect_response(response, self)
-        ng_click=response.xpath("//a[@data-name='Mobiltelefoner'][@data-category='topmenu'][@class='tracking_link_topmenu']/@ng-click").extract_first()
-        mobiler_url=response.urljoin(ng_click.split("'")[1])
+        #Now let's find URL for page with mobile phones for sale
+        ng_click = response.xpath("//a[@data-name='Mobiltelefoner']\
+                                  [@data-category='topmenu']\
+                                  [@class='tracking_link_topmenu']/@ng-click")\
+                                  .extract_first()
+        mobiler_url = response.urljoin(ng_click.split("'")[1])
         self.logger.info(mobiler_url)
+        
         yield SplashRequest(url=mobiler_url, 
                             callback=self.parse_list_of_mobiles, 
                             endpoint="execute",
                             args={"lua_source": script})
         
     def parse_list_of_mobiles(self, response):
-        #self.logger.info("Inde i parse_list_of_mobiles, url'en er {}".format(response.url))
         script = """
         function main(splash, args)
           assert(splash:go(args.url))
@@ -121,9 +112,11 @@ class MobilerSpider(scrapy.Spider):
           }
         end
         """
+        
+        #Scrape each individual phone page
         for phone in response.xpath("//button[@ng-href]"):
-            #print(phone.xpath("@ng-href").extract_first())
-            phone_page=(response.urljoin(phone.xpath("@ng-href").extract_first()))
+            phone_page=(response.urljoin(
+                    phone.xpath("@ng-href").extract_first()))
             self.logger.info(phone_page)
             yield SplashRequest(url=phone_page, 
                                 callback=self.parse_single_mobile, 
@@ -131,178 +124,29 @@ class MobilerSpider(scrapy.Spider):
                                 args={"lua_source": script})
 
     def parse_single_mobile(self, response):
-        brand, storage, model, price = None, None, None, None
+        #Strip relevant data for each phone
         phone_timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        #if "iphone-6s" in response.url.lower():
-        #    inspect_response(response, self)
-        #if response.xpath(
-        #        "//div[@class='product-overview ng-scope']//h1/text()")\
-        #        .extract_first() == "Mate 10 Lite":
-        #            inspect_response(response, self)
         brand = response.xpath(
-                "//div[@class='product-overview ng-scope']//h3/text()").extract_first()
-        storage = response.xpath(
-            '//*[@id="menufication-page-holder"]/div[1]/div/div/div[5]/div/div/div/div[2]/div[1]/div/h3/text()'
-            ).extract_first()
-        model=response.xpath(
-                "//div[@class='product-overview ng-scope']//h1/text()").extract_first()
-        try:
-            price_string=unicodedata.normalize("NFKD", 
-                                           response.xpath(
+                "//div[@class='product-overview ng-scope']//h3/text()")\
+                .extract_first()
+        #The crazy Xpath selector below is found in Chrome Dev Tools
+        storage = response.xpath('//*[@id="menufication-page-holder"]\
+                                 /div[1]/div/div/div[5]/div/div/div/div[2]\
+                                 /div[1]/div/h3/text()')\
+                                 .extract_first()
+        model=response.xpath("//div[@class='product-overview ng-scope']\
+                             //h1/text()").extract_first()
+        price_string=unicodedata.normalize("NFKD", response.xpath(
                 "//text()[contains(.,'Telefonens pris')]").extract_first())
-            price=int(''.join(filter(str.isdigit, price_string)))
-        except TypeError:
-            #price = "<UNKNOWN>"
-            price = None
-        mobile={"brand": brand, 
-                "storage": storage, 
-                "model": model, 
-                "price": price,
-                "timestamp": phone_timestamp}
+        price = int(''.join(filter(str.isdigit, price_string)))
+        mobile = {"brand": brand, 
+                  "storage": storage, 
+                  "model": model, 
+                  "price": price,
+                  "timestamp": phone_timestamp}
         self.logger.info(
-                "\n\nBrand: {0}, Model:{1}, Storage:{2}, Price:{3}, Timestamp:{4}\n".format(brand, model, storage, price, phone_timestamp))
+                "\n\nBrand: {0}, Model:{1}, Storage:{2}, Price:{3}, \
+                Timestamp:{4}\n".format(brand, model, storage, price, 
+                phone_timestamp))
         self.phones.append(mobile)
 
-def load_phones():
-    JSON_FILE="scraped_cbb_phones.json"
-    try:
-        with open(JSON_FILE, "r") as fp:
-            phones=json.load(fp)
-        return phones
-    except FileNotFoundError:
-        return []
-    
-
-def save_phones(phones):
-    JSON_FILE="scraped_cbb_phones.json"
-    with open(JSON_FILE, "w") as fp:
-        json.dump(phones, fp)
-        
-        
-"""
-LUA SCRIPT
-function main(splash, args)
-  assert(splash:go(args.url))
-  assert(splash:wait(2.5))
-  splash:set_viewport_size(1366, 4000)
-  local element = splash:select("#menufication-page-holder > div.landingpage.item-template.overview-template > div.products-section > div > div > div.col-xs-12.text-center > button")
-  while element do
-    element:mouse_click()
-    splash:wait(2)
-  end  
-  --local bounds = element:bounds()
-  --assert(element:mouse_click{x=bounds.width/2, y=bounds.height/2})
-  assert(element:mouse_click())
-  assert(splash:wait(1))
-  return {
-    html = splash:html(),
-    png = splash:png(),
-    har = splash:har(),
-  }
-end
-"""
-
-
-
-
-
-"""
-FAILS ON 6TH CLICK, OK IF ONLY 5 CLICKS
-function main(splash, args)
-  assert(splash:go(args.url))
-  assert(splash:wait(1.5))
-  --splash:set_viewport_full()
-  splash:set_viewport_size(1024, 5500)  
-  element = splash:select("#menufication-page-holder > div.landingpage.item-template.overview-template > div.products-section > div > div > div.col-xs-12.text-center > button")
-	ok, reason = element:mouse_click()
- 	splash:wait(1)
-	ok, reason = element:mouse_click()
- 	splash:wait(1)
-	ok, reason = element:mouse_click()
- 	splash:wait(1)
-	ok, reason = element:mouse_click()
- 	splash:wait(1)
-	ok, reason = element:mouse_click()
- 	splash:wait(1)
-	ok, reason = element:mouse_click()
- 	splash:wait(1)
-  
-  return {
-    html = splash:html(),
-    --png = splash:png(),
-    --har = splash:har(),
-  }
-end"""
-
-
-"""
-function main(splash, args)
-  assert(splash:go(args.url))
-  assert(splash:wait(1.5))
-  --splash:set_viewport_full()
-  splash:set_viewport_size(1024, 5500)  
-  element = splash:select("#menufication-page-holder > div.landingpage.item-template.overview-template > div.products-section > div > div > div.col-xs-12.text-center > button")
-	ok, reason = element:mouse_click()
-  splash:wait(1)
-  ok, reason = element:mouse_click()
-  splash:wait(1)
-  ok, reason = element:mouse_click()
-  splash:wait(1)
-  --ok, reason = element:mouse_click()
-  --splash:wait(1)
-  --ok, reason = element:mouse_click()
- 	--splash:wait(1)
-  --ok, reason = element:mouse_click()
- 	--splash:wait(1)
-
---  while ok do
---    ok, reason = element:mouse_click()
-    --ok, reason = element:mouse_click()
--- 		splash:wait(1)
-    --ok = nil
---  end
-	--ok, reason = element:mouse_click()
- 	--splash:wait(1)
-  return {
-    --html = splash:html(),
-    png = splash:png(),
-    --har = splash:har(),
-    json = {
-      --key1 = splash:evaljs("document.pfCtrl.limit"), 
-      key3 = splash:evaljs("document.title"),
-      key2=ok}
-  }
-end
-"""
-
-
-"""
-THIS WORKS!
-function main(splash, args)
-  assert(splash:go(args.url))
-  assert(splash:wait(2.5))
-  --splash:set_viewport_full()
-  splash:set_viewport_size(1024, 5500)  
-  element = splash:select("#menufication-page-holder > div.landingpage.item-template.overview-template > div.products-section > div > div > div.col-xs-12.text-center > button")
-  more_to_show = true
-  while more_to_show do
-  	if pcall(show_more, splash, element) then
-      more_to_show = true
-  	else
-      more_to_show = false
-  	end
-  end
-
-  return {
-    html = splash:html(),
-  }
-end
-
-function show_more(splash, element)
-  ok, reason = element:mouse_click()
-  splash:wait(2)
-end  
-"""
-
-#Possible Xpath to find storage for all phones (including Doro)
-#//*[@id="menufication-page-holder"]/div[1]/div/div/div[5]/div/div/div/div[2]/div[1]/div/h3/text()
--- cbb/spiders/tools.py
+++ cbb/spiders/tools.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import json
+
 """
 Created on Sun Nov 19 19:04:08 2017
 
@@ -85,4 +87,22 @@ def find_changed_phones(new_phones, all_phones):
             changed_phones_since_last_run.append(n)
     return changed_phones_since_last_run
             
+def phone_sorter(phone):
+    return (phone["brand"], phone["model"], phone["price"])
+
+def load_phones():
+    JSON_FILE="scraped_cbb_phones.json"
+    try:
+        with open(JSON_FILE, "r") as fp:
+            phones=json.load(fp)
+        return phones
+    except FileNotFoundError:
+        return []
+    
+
+def save_phones(phones):
+    JSON_FILE="scraped_cbb_phones.json"
+    with open(JSON_FILE, "w") as fp:
+        json.dump(phones, fp)
+        
         
