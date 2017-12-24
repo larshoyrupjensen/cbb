977da04e12be3a10927f80b2bc930764a4b96b94
larshoyrupjensen <lars.hoyrup.jensen@gmail.com>
2017-12-24 15:46:09 +0100

    added phone.py

---
+++ cbb/spiders/phone.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 24 15:34:47 2017
+
+Classes and functions to find necessary information in phone history
+
+@author: Lars
+"""
+
+JSON_FILE= "../../scraped_cbb_phones.json"
+
+class Phone:
+    
+    def __init__(self):
+        pass
+    
+    brand =
+    model =
+    storage =
+    price =
+    
